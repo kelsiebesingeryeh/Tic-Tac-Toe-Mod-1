250ea90fd8afee13af9ae53294f9ab88ce5cf5cb
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-11-07 16:40:46 -0700

    Add functionality to the DOM in order for game to appear

--- game.js
+++ game.js
@@ -1,13 +1,74 @@
+
 class Game {
   constructor() {
-    
+    this.player1 = new Player(1, 'taxi', true, false);
+    this.player2 = new Player(2, 'pizza', false, false);
+    this.boardData = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    this.winConditions = [
+      [0, 1, 2], //row winning
+      [3, 4, 5], //row winning
+      [6, 7, 8], //row winning
+      [0, 3, 6], //column winning
+      [1, 4, 7], //column winning
+      [2, 5, 8], //column winning
+      [0, 4, 8], //diagonal winning
+      [2, 4, 6], //diagonal winning
+    ];
+    this.drawConditions //need to come up with draw conditions here
+    this.gameWon = false;
+    this.clickCounter = 0;
   }
+
+  addMovesToBoard(boxIndex) {
+    for (var i = 0; i < this.boardData.length; i++) {
+        this.boardData.push(boxIndex);
+      }
+    // if this boxIndex matches
+    // once that box is clicked, then push that number into the game board array at that index.
+  }
+
+  switchPlayerTurn() {
+    this.clickCounter++;
+    this.player1.turn = !this.player1.turn;
+    this.player2.turn = !this.player2.turn;
+  }
+
+  checkWins() {
+    if (this.clickCounter > 5) {
+      for (var i = 0; i < this.winConditions.length; i++) {
+        if (this.winConditions[i] === this.boardData) {
+          return this.gameWon = true;
+        } else {
+          //do nothing
+        }
+    }
+   }
+  }
+
+  resetBoard() {
+    //if statement - when the game is over, reset the board data and clear inner text
+        this.boardData = [];
+      }
 }
 
-var player1 = new Player()
-var player2 = new Player()
+//after every turn, we want to check the wins to see if a player has won
+// this.boardData[boxIndex] //exactly where in the array to manipulate
+
+  // check win conditions?- loop through win conditions and compare to board DATA. if/else stricly compare to win conditions
+  // winning conditions can match board data (loop through winning conditions through board data). same as draw conditions as well.
+  // compare board conditions to winning conditions on every turn to see if the game is over
+
+// number of boxes in hTML is the same number of zeros in board data, based on which one is clicked, there is a number assoicated with switchPlayerTurn
+// event.target is 1, take in 1, change first index,
+// function needs to take an argument of a number, number that represents an index in board DATA
+
+// FUNCTION?!
+// keep track of data for game board after player turn
 
 
-/*
-Goal: A Way to Keep Track of the data for the game board
-*/
+// detect when game is a draw - need to figure out draw conditions and every time a player goes, it checks draw conditions
+// save winning game board to correct players win array - do this last. saving wins to local storage
+// reset game to clear board - clear out board data. reset to what it is when its instantiated. []
+
+
+// how to create the board within the game class and reassign the values in each space
