d1e173ae10626f4e77d8c6fa076e7b709f32daae
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-11-08 18:22:03 -0700

    Add function to display game board data from data model

--- game.js
+++ game.js
@@ -15,26 +15,23 @@ class Game {
     ];
     this.gameWon = false;
     this.clickCounter = 0;
+    this.tie = false;
   }
 
   addMovesToBoardData(boxIndex) {
     var newIndex = boxIndex.split('-');
     newIndex = parseInt(newIndex[1]);
     if (this.player1.turn === true) {
-      console.log('player1', this.player1.turn)
-      this.boardData.splice(newIndex, 1, this.player2.token);
-    } else if (this.player2.turn === true) {
-      console.log('player2', this.player2.turn)
       this.boardData.splice(newIndex, 1, this.player1.token);
+    } else if (this.player2.turn === true) {
+      this.boardData.splice(newIndex, 1, this.player2.token);
     }
   }
 
   switchPlayerTurn() {
     this.clickCounter++;
     this.player1.turn = !this.player1.turn;
-    console.log('player1', this.player1.turn)
     this.player2.turn = !this.player2.turn;
-    console.log('player2', this.player2.turn)
   }
 
   checkWins() {
@@ -69,9 +66,11 @@ class Game {
       } else if (oCount === 3) {
         this.player2.winner = true;
         return this.gameWon = true;
-      } else if (this.clickCounter === 9) {
-        return this.gameWon = false;
       }
+      }
+      if (this.clickCounter === 9) {
+        this.tie = true;
+        return this.gameWon = false;
     }
 }
 
@@ -82,36 +81,18 @@ class Game {
       this.player2.wins++;
     }
   }
+
+  resetBoard() {
+        this.player1.turn = true;
+        this.player2.turn = false;
+        this.boardData = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        this.gameWon = false;
+        this.clickCounter = 0;
+      }
 }
 
-
-//loop through board data and if that index == X and add i to the x array
-// if boardData[i] = add to the ) array
-// then from there you can compare each of those to the winning combos
-
-// resetBoard() {
-//   //if statement - when the game is over, reset the board data and clear inner text
-//       this.boardData = [];
-//     }
-
-//after every turn, we want to check the wins to see if a player has won
-// this.boardData[boxIndex] //exactly where in the array to manipulate
 
 // check win conditions?- loop through win conditions and compare to board DATA. if/else stricly compare to win conditions
 // winning conditions can match board data (loop through winning conditions through board data). same as draw conditions as well.
 // compare board conditions to winning conditions on every turn to see if the game is over
-
-// number of boxes in hTML is the same number of zeros in board data, based on which one is clicked, there is a number assoicated with switchPlayerTurn
-// event.target is 1, take in 1, change first index,
-// function needs to take an argument of a number, number that represents an index in board DATA
-
-// FUNCTION?!
-// keep track of data for game board after player turn
-
-
 // detect when game is a draw - need to figure out draw conditions and every time a player goes, it checks draw conditions
-// save winning game board to correct players win array - do this last. saving wins to local storage
-// reset game to clear board - clear out board data. reset to what it is when its instantiated. []
-
-
-// how to create the board within the game class and reassign the values in each space
--- main.js
+++ main.js
@@ -11,46 +11,46 @@ boardWrapper.addEventListener('click', function(event) {
   if (event.target.classList.contains('box')) {
     playGame();
   }
-});
+})
 
 function startGame() {
   currentGame = new Game();
   currentGame.player1.turn = true;
-  console.log('start game', currentGame.player1.turn)
   defaultDisplay();
   displayWinnerCount();
 }
 
 function playGame() {
   toggleToken(event);
+  addMoves(event);
   currentGame.switchPlayerTurn();
   updateDisplayPlayerTurn();
   disableClicks(event);
-  addMoves(event);
   currentGame.checkWins();
   displayWinner();
   displayDraw();
-  displayWinnerCount()
+  displayWinnerCount();
+  // setTimeOut(clearBoard, 5000);
 }
 
 function displayWinnerCount() {
-  currentGame.saveWins()
+  currentGame.saveWins();
   player1Wins.innerText = `${currentGame.player1.wins} wins`;
   player2Wins.innerText = `${currentGame.player2.wins} wins`;
+  console.log(currentGame.player1.wins)
+  console.log(currentGame.gameWon)
 }
 
 function displayWinner() {
-  // currentGame.player1.token = '🚕';
-  // currentGame.player2.token = '🍕';
   if (currentGame.gameWon === true && currentGame.player1.turn === false) {
-    playerDisplayText.innerText = `${currentGame.player1.token} won!`;
+    playerDisplayText.innerText = `🚕 won!`;
   } else if (currentGame.gameWon === true && currentGame.player2.turn === false) {
-    playerDisplayText.innerText = `${currentGame.player2.token} won!`;
+    playerDisplayText.innerText = `🍕 won!`;
   }
 }
 
 function displayDraw() {
-  if (currentGame.clickCounter === 9 && currentGame.gameWon === false) {
+  if (currentGame.clickCounter === 9 && currentGame.tie === true) {
     playerDisplayText.innerText = `It's a draw!`;
   }
 }
@@ -69,63 +69,43 @@ function disableClicks(event) {
 function addMoves(event) {
   boxIndex = event.target.id;
   currentGame.addMovesToBoardData(boxIndex);
-  console.log(currentGame.boardData);
 }
 
 function toggleToken(event) {
-  // currentGame.player1.token = '🚕';
-  // currentGame.player2.token = '🍕';
   if (currentGame.player1.turn === true) {
-    event.target.innerText = `${currentGame.player1.token}`
-    console.log('player1', currentGame.player1.turn)
+    event.target.innerText = `🚕`;
   } else if (currentGame.player2.turn === true) {
-    event.target.innerText = `${currentGame.player2.token}`
-    console.log('player2', currentGame.player2.turn)
+    event.target.innerText = `🍕`;
   }
 }
 
 function defaultDisplay() {
   if (currentGame.player1.turn === true) {
-    console.log('player1', currentGame.player1.turn)
-    playerDisplayText.innerText = `It's ${currentGame.player1.token}'s turn`;
+    playerDisplayText.innerText = `It's 🚕's turn`;
   }
 }
 
 function updateDisplayPlayerTurn() {
-  // currentGame.player1.token = '🚕';
-  // currentGame.player2.token = '🍕';
   if (currentGame.player1.turn === true) {
-    console.log('player1', currentGame.player1.turn)
-    playerDisplayText.innerHTML = `It's ${currentGame.player1.token}'s turn`;
+    playerDisplayText.innerHTML = `It's 🚕's turn`;
   } else if (currentGame.player2.turn === true) {
-    console.log('player2', currentGame.player2.turn)
-    playerDisplayText.innerHTML = `It's ${currentGame.player2.token}'s turn`;
+    playerDisplayText.innerHTML = `It's 🍕's turn`;
   }
 }
 
 function clearBoard() {
-  for (var i = 0; i < allBoxes.length; i++) {
-      allBoxes[i] = '';
-    }
-    //add timeout here
-  currentGame.resetBoard();
+  if (currentGame.gameWon === true || currentGame.tie === true) {
+    for (var i = 0; i < allBoxes.length; i++) {
+        allBoxes[i].innerText = '';
+        allBoxes[i].classList.remove('avoid-clicks');
+      }
+    currentGame.resetBoard();
+    defaultDisplay();
+  }
 }
 
 /*
 QUESTIONS - NEED HELP
-1. need to be able to turn the token text into an icon
-2. need to add the win count to the side of the board - connect the data there to count the winners
 3. timeout - need to set a timeout to clear the board after game ends. redraw board once it refreshes
 4. localStorage!!!
-
-//currentGame.player1.turn .token - calling currentGame - everytime you are referencing that class
-
-Connect ID from player JS to Game js (one or two to the game) - who are we worreid about
-- which player is playing
-- array to store data from the Game
--zero means nothing is in that placement
--1 is a taxi
--2 is a pizza
-var newArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-manipulating the DOM and push into innerText
 */
